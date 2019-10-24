a7d864f6e6b465bbd55de012d2c043cf854d9504
Aaron Parecki <aaron@parecki.com>
2019-10-24 16:45:31 +0200

    add local jquery

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,10 +12,7 @@
 
     @yield('headtags')
 
-    <script
-      src="https://code.jquery.com/jquery-3.4.1.min.js"
-      integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-      crossorigin="anonymous"></script>
+    <script src="/jquery/jquery-1.12.0.min.js"></script>
 
     <link href="/bulma-0.7.5/bulma.min.css" rel="stylesheet">
 
