d0fce9fdd6a81fbb497efddcf8337ea6a25a77d8
Aaron Parecki <aaron@parecki.com>
2019-11-15 18:10:17 -0800

    better display of webmentions

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts/main')
 
+@section('headtags')
+<link rel="webmention" href="/webmention">
+@endsection
+
 @section('content')
 <section class="section">
 
@@ -11,27 +15,27 @@
 
     <div class="buttons has-addons with-dropdown">
 
-      <a href="{{ route('edit-event', $event) }}" class="button">
-        <span class="icon">@icon(edit)</span>
-        <span>Edit</span>
-      </a>
+        <a href="{{ route('edit-event', $event) }}" class="button">
+            <span class="icon">@icon(edit)</span>
+            <span>Edit</span>
+        </a>
 
-      <div class="dropdown is-right">
-        <div class="dropdown-trigger">
-          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-            <span class="icon is-small">@icon(angle-down)</span>
-          </button>
-        </div>
+        <div class="dropdown is-right">
+            <div class="dropdown-trigger">
+                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                    <span class="icon is-small">@icon(angle-down)</span>
+                </button>
+            </div>
 
-        <div class="dropdown-menu" id="dropdown-menu" role="menu">
-          <div class="dropdown-content">
-            <a class="dropdown-item" href="{{ route('clone-event', $event) }}">
-              <span class="icon">@icon(copy)</span>
-              <span>Clone Event</span>
-            </a>
-          </div>
+            <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                <div class="dropdown-content">
+                    <a class="dropdown-item" href="{{ route('clone-event', $event) }}">
+                        <span class="icon">@icon(copy)</span>
+                        <span>Clone Event</span>
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
 
     </div>
 
@@ -50,7 +54,7 @@
         <span>
           {!! $event->display_date() !!}
           @if(!$event->is_multiday())
-            <br><span class="time">{!! $event->display_time() !!}</span>
+              <br><span class="time">{!! $event->display_time() !!}</span>
           @endif
         </span>
     </div>
@@ -76,7 +80,7 @@
 
     <div class="segment tags are-medium" id="tags">
         @foreach($event->tags as $tag)
-          <a href="{{ $tag->url() }}" class="tag is-rounded">#{{ $tag->tag }}</a>
+            <a href="{{ $tag->url() }}" class="tag is-rounded">#{{ $tag->tag }}</a>
         @endforeach
     </div>
 
@@ -85,16 +89,18 @@
             <h2 class="subtitle">RSVPs</h2>
 
             @if(Auth::user())
-              <button id="rsvp-button" class="button {{ $event->rsvp_string_for_user(Auth::user()) == 'yes' ? 'is-primary' : '' }}" data-action="{{ route('event-rsvp', $event->id) }}">I'm Going!</button>
-              <br><br>
+                <button id="rsvp-button" class="button {{ $event->rsvp_string_for_user(Auth::user()) == 'yes' ? 'is-primary' : '' }}" data-action="{{ route('event-rsvp', $event->id) }}">I'm Going!</button>
+                <br><br>
             @endif
 
             <ul>
                 @foreach($event->rsvps as $rsvp)
                     @if($rsvp->rsvp == 'yes')
                     <li class="avatar">
-                      <img src="{{ $rsvp->author()['photo'] }}" width="48">
-                      <a href="{{ $rsvp->author()['url'] }}">{{ $rsvp->author()['name'] ?? p3k\url\display_url($rsvp->author()['url']) }}</a>
+                        @if($rsvp->author()['photo'])
+                            <img src="{{ $rsvp->author()['photo'] }}" width="48">
+                        @endif
+                        <a href="{{ $rsvp->author()['url'] }}">{{ $rsvp->author()['name'] ?? p3k\url\display_url($rsvp->author()['url']) }}</a>
                     </li>
                     @endif
                 @endforeach
