dab354f7637b253fc6c3b6e300bce1e47c4b3287
Aaron Parecki <aaron@parecki.com>
2019-12-19 15:25:03 -0800

    fix showing author url when no name or photo

--- resources/views/components/rsvp-avatar.blade.php
+++ resources/views/components/rsvp-avatar.blade.php
@@ -2,5 +2,5 @@
     @if($rsvp->author()['photo'])
         <img src="@image_proxy($rsvp->author()['photo'], '96x96,sc')" width="48" class="photo">
     @endif
-    <a href="{{ $rsvp->source_url ?: $rsvp->author()['url'] }}">{{ $rsvp->author()['name'] ?? p3k\url\display_url($rsvp->author()['url']) }}</a>
+    <a href="{{ $rsvp->source_url ?: $rsvp->author()['url'] }}">{{ $rsvp->author()['name'] ?: p3k\url\display_url($rsvp->author()['url']) }}</a>
 </span>
