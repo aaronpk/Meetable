e85052a9eb8c77aa1dfcde3c4c0376e912d13f1b
Aaron Parecki <aaron@parecki.com>
2020-01-26 18:17:48 -0800

    set default string size

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Blade::directive('icon', function ($expression) {
             return '<svg class="svg-icon"><use xlink:href="/font-awesome-5.11.2/sprites/solid.svg#<?php echo "'.$expression.'" ?>"></use></svg>';
         });
