2e98a3657a833cd6f446b661cb43fa915d1342b2
Aaron Parecki <aaron@parecki.com>
2020-01-28 10:58:19 -0800

    check for vendor folder and show an error if it doesn't exist
    for #44

--- public/index.php
+++ public/index.php
@@ -1,53 +1,17 @@
 <?php
-
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
+// Check if the dependencies are installed. If someone does a git clone without then
+// installing the dependencies, we can tell them to do so. If they download a release,
+// the dependencies should have already come with that download.
+if(!file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require_once(__DIR__.'/../resources/setup/missing-dependencies.php');
+    die();
+}
 
 require __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
-
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
---
+++ resources/setup/missing-dependencies.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Meetable</title>
+
+        <link href="/bulma-0.8.0/bulma.min.css" rel="stylesheet">
+
+        <link href="/assets/style.css" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md" style="font-weight: normal;">
+                    Meetable
+                </div>
+
+                <article class="message is-danger" style="max-width: 600px;">
+                    <div class="message-header">Missing Dependencies</div>
+                    <div class="message-body content">
+                        <p>The <code>vendor</code> folder is missing. This likely happened because you installed a development version from git and have not yet run <code>composer install</code>.</p>
+                        <p>You can alternatively download a <a href="https://github.com/aaronpk/Meetable/releases">release version</a> and it will include everything you need.</p>
+                    </div>
+                </article>
+
+            </div>
+        </div>
+    </body>
+</html>
