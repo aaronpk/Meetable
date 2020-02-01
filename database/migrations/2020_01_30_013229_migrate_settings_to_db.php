0aff01b9b5bf458dbb205a59ed50d4690f0d6753
Aaron Parecki <aaron@parecki.com>
2020-02-01 10:02:36 -0800

    only show migration notice if settings were migrated

--- database/migrations/2020_01_30_013229_migrate_settings_to_db.php
+++ database/migrations/2020_01_30_013229_migrate_settings_to_db.php
@@ -14,10 +14,13 @@ class MigrateSettingsToDb extends Migration
      */
     public function up()
     {
+        $migrated = false;
+
         $properties = ['LOGO_URL', 'LOGO_WIDTH', 'LOGO_HEIGHT', 'FAVICON_URL', 'GA_ID'];
         foreach($properties as $id) {
             if(env($id)) {
                 Setting::set(strtolower($id), env($id));
+                $migrated = true;
             }
         }
 
@@ -25,16 +28,22 @@ class MigrateSettingsToDb extends Migration
             'AUTH_SHOW_LOGIN', 'AUTH_SHOW_LOGOUT'];
         foreach($checkboxes as $id) {
             Setting::set(strtolower($id), env($id) ? 1 : 0);
+            if(env($id))
+                $migrated = true;
         }
 
         $passwords = ['GOOGLEMAPS_API_KEY', 'TWITTER_CONSUMER_KEY', 'TWITTER_CONSUMER_SECRET',
             'TWITTER_ACCESS_TOKEN', 'TWITTER_ACCESS_TOKEN_SECRET'];
         foreach($passwords as $id) {
             Setting::set(strtolower($id), env($id));
+            if(env($id))
+                $migrated = true;
         }
 
-        Log::info('You can delete the following entries from your .env file:');
-        Log::info(implode(', ', array_merge($properties, $checkboxes, $passwords)));
+        if($migrated) {
+            Log::info('You can delete the following entries from your .env file:');
+            Log::info(implode(', ', array_merge($properties, $checkboxes, $passwords)));
+        }
     }
 
     /**
