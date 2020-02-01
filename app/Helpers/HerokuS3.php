4010532e782e5564f11c8371aeec224303154578
Aaron Parecki <aaron@parecki.com>
2020-02-01 12:02:52 -0800

    avoid any parsing if no CLOUDCUBE_URL is set

--- app/Helpers/HerokuS3.php
+++ app/Helpers/HerokuS3.php
@@ -5,17 +5,23 @@ namespace App\Helpers;
 class HerokuS3
 {
     public static function get_aws_root_from_env() {
+        if(!env('CLOUDCUBE_URL')) return null;
+
         $value = basename(Uri::get_uri_path(env('CLOUDCUBE_URL')));
         return empty($value) ? null : $value;
     }
 
     public static function get_aws_url_from_env() {
+        if(!env('CLOUDCUBE_URL')) return null;
+
         $path = Uri::get_uri_path(env('CLOUDCUBE_URL'));
         $value = Uri::get_uri_without_path(env('CLOUDCUBE_URL'), $path);
         return empty($value) ? null : $value;
     }
 
     public static function get_default_aws_region() {
+        if(!env('CLOUDCUBE_URL')) return null;
+
         $domainParts = Uri::get_uri_domain_parts(
             Uri::get_uri_host(env('CLOUDCUBE_URL'))
         );
@@ -29,6 +35,8 @@ class HerokuS3
     }
 
     public static function get_aws_bucket_from_env() {
+        if(!env('CLOUDCUBE_URL')) return null;
+
         $domainParts = Uri::get_uri_domain_parts(
             Uri::get_uri_host(env('CLOUDCUBE_URL'))
         );
