03f8ec3e9a364b6afb583cd0d4760fe227adde21
Lewis Cowles <lewiscowles@me.com>
2020-02-01 14:59:57 +0000

    Filesystems.php modifications
    see http://sandbox.onlinephpfunctions.com/code/09ca54d45535faff5be3ec10b4b01a18b4877884

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,13 +57,34 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'key' => env('AWS_ACCESS_KEY_ID', env('CLOUDCUBE_ACCESS_KEY_ID')),
+            'secret' => env('AWS_SECRET_ACCESS_KEY', env('CLOUDCUBE_SECRET_ACCESS_KEY')),
+            'region' => env('AWS_DEFAULT_REGION', HerokuS3::get_default_aws_region()),
+            'bucket' => env('AWS_BUCKET', HerokuS3::get_aws_bucket_from_env()),
+            'url' => env('AWS_URL', HerokuS3::get_aws_url_from_env()),
+            'root' => env('AWS_ROOT', HerokuS3::get_aws_root_from_env()),
         ],
 
     ],
 
 ];
+
+function get_url_path($url) {
+    $parsed = parse_url($url);
+    return array_key_exists('path', $parsed) ? $parsed['path'] : '';
+}
+
+function get_url_without_path($url, $path=null) {
+    $path = $path ?? '';
+    return substr($url, 0, strlen($url) - strlen($path));
+}
+
+function get_aws_bucket_from_env() {
+    $value = basename(get_url_path(env('AWS_URL')));
+    return empty($value) ? null : $value;
+}
+function get_aws_url_from_env() {
+    $path = get_url_path(env('AWS_URL'));
+    $value = get_url_without_path(env('AWS_URL'), $path);
+    return empty($value) ? null : $value;
+}
