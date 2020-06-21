3bf4459e68d624af2e6005aee5a1745742499ee3
Aaron Parecki <aaron@parecki.com>
2020-06-20 17:40:41 -0700

    add placeholder image when no avatar is available
    closes #104

--- resources/views/components/rsvp-avatar.blade.php
+++ resources/views/components/rsvp-avatar.blade.php
@@ -2,7 +2,7 @@
     @if($rsvp->author_photo())
         <img src="{{ $rsvp->author_photo() }}" width="48" class="photo">
     @else
-        <span class="photo"></span>
+        <img src="/images/placeholder.png" width="48" class="photo">
     @endif
     @if($rsvp->rsvp_link())
         <a href="{{ $rsvp->rsvp_link() }}">{{ $rsvp->author_display_name() }}</a>
