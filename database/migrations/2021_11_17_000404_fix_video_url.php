da43317da7a5dba60cf23d8e141a619b2b3709b9
Aaron Parecki <aaron@parecki.com>
2021-11-16 16:05:45 -0800

    fix nullable for video_url column

---
+++ database/migrations/2021_11_17_000404_fix_video_url.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixVideoUrl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->longtext('video_url')->nullable()->default('')->change();
+        });
+        Schema::table('event_revisions', function (Blueprint $table) {
+            $table->longtext('video_url')->nullable()->default('')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->longtext('video_url')->nullable(false)->default('')->change();
+        });
+        Schema::table('event_revisions', function (Blueprint $table) {
+            $table->longtext('video_url')->nullable(false)->default('')->change();
+        });
+    }
+}
