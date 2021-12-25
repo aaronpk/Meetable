354bb8994aa2b38944b631ce2710572e1f966bd3
Martijn van der Ven <martijn@vanderven.se>
2021-12-25 16:22:17 +0100

    Add tests for expected behaviour

---
+++ tests/Feature/ICSContentNegotiationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ICSContentNegotiationTest extends TestCase
+{
+    public function testContentNegotiation()
+    {
+        // Request with default header (text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8) results in the ICS view, HTML.
+        $response = $this->get('/ics/2021/12/indieweb-create-day-vfsXwP8OTgkK.ics');
+        $response->assertViewIs('ics');
+        // Request with specific text/calendar header results in the ICS file.
+        $response = $this->withHeaders(['Accept' => 'text/calendar'])->get('/ics/2021/12/indieweb-create-day-vfsXwP8OTgkK.ics');
+        $response->assertOk();
+        $response->assertSee('BEGIN:VCALENDAR');
+        // Request accepting anything results in the ICS file.
+        $response = $this->withHeaders(['Accept' => '*/*'])->get('/ics/2021/12/indieweb-create-day-vfsXwP8OTgkK.ics');
+        $response->assertOk();
+        $response->assertSee('BEGIN:VCALENDAR');
+    }
+}
