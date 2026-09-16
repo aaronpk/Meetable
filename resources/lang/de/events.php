d5423b09bd8d9ecf37543fcbf6176ef8b6cf958f
Aaron Parecki <aaron@parecki.com>
2026-09-16 17:29:13 +0200

    chore(l10n): add German translation
    Translation: Meetable/Meetable
    Language: German
    Translate-URL: https://hosted.weblate.org/projects/meetable/meetable/de/

---
+++ resources/lang/de/events.php
@@ -0,0 +1,72 @@
+<?php
+return [
+    'meta' => [
+        /*
+ * Event pages, event lists, the archive and tag pages.
+ */
+        // Labels for the date and location in link previews
+        'date' => '',
+        'location' => '',
+    ],
+    'actions' => [
+        // The menu of actions for people who can edit the event
+        'clone' => '',
+        'create_recurring' => '',
+        'add_photo' => '',
+        'add_sub_event' => '',
+        'edit_responses' => '',
+        'configure_registration' => '',
+        'enable_registration' => '',
+        'revision_history' => '',
+        'moderate_responses' => '',
+        'copy_export_url' => '',
+    ],
+    'meeting_url_needs_timezone' => '',
+    'status' => [
+        'confirmed' => '',
+        'tentative' => '',
+        'postponed' => '',
+        'cancelled' => '',
+        'live_now' => '',
+    ],
+    // Shown before the original date of a postponed event
+    'tbd_originally' => '',
+    'calendar' => [
+        'add_to_calendar' => '',
+        'tag_feed' => '',
+        'single_event' => '',
+        'ical' => '',
+        'google' => '',
+    ],
+    'code_of_conduct' => '',
+    'join_meeting' => '',
+    'meeting_link_shown_soon' => '',
+    'notes' => '',
+    'sessions' => '',
+    // Event lists
+    'no_events' => '',
+    'no_upcoming_events' => '',
+    'past_events' => '',
+    'unlisted_events' => '',
+    'tag_archive' => '',
+    'icalendar_feed' => '',
+    'event_count' => '',
+    'title' => [
+        // Page titles. :site is the name of the website
+        'year' => '',
+        'year_on_site' => '',
+        'month' => '',
+        'day' => '',
+    ],
+    'local_time' => [
+        // The page that shows an event's time in other timezones
+        'invalid_input' => '',
+        'event_time' => '',
+        'your_local_time' => '',
+    ],
+    'subscribe' => [
+        // The page for subscribing to an iCalendar feed
+        'title' => '',
+        'instructions' => '',
+    ],
+];
